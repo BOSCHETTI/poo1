9deaccca88b85f8271771f5db891c7d98409feca
Aldo Boschetti Filho <abf214@gmail.com>
2021-10-12 14:39:17 -0300

    first commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO1
+{
+    class ContaBancaria
+    {
+        public string Conta;
+        public string Agencia;
+        public string Nome;
+        private string Senha = "123";
+        private int Saldo;
+
+        public ContaBancaria(int saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Depositar(int valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Sacar(int valor)
+        {
+            Saldo = Saldo - valor;
+        }
+
+        public int ConsultatSaldo()
+        {
+            int taxa = 10;
+            int valorTaxa = (Saldo * taxa) / 100;
+            return Saldo - valorTaxa;
+        }
+
+        public void ImprimirSaldo()
+        {
+            Console.WriteLine($"(Nome)  o saldo na sua conta (Conta) na agência (Agencia) é: {ConsultatSaldo()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace POO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria(200);
+
+            contaBancaria.Agencia = "1234-5";
+            contaBancaria.Conta = "147582-5";
+            contaBancaria.Nome = "Aldo Boschetti Filho";
+
+            contaBancaria.ImprimirSaldo();
+
+            contaBancaria.Depositar(200);
+
+            contaBancaria.ImprimirSaldo();
+
+            contaBancaria.Depositar(300);
+
+            contaBancaria.ImprimirSaldo();
+
+            contaBancaria.Sacar(100);
+
+            contaBancaria.ImprimirSaldo();
+
+        }
+    }
+} 
